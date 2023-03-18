1cb8c77a6e6fcff4a8d37be8d0e8412580b4fa85
Carlos <103267914+Carlos-H1307@users.noreply.github.com>
2023-03-18 18:45:26 -0300

    Carlos - Commit 1
    Carrinho de compras e produtos

---
+++ js/carrinho.js
@@ -0,0 +1,16 @@
+import {produtos} from "./produtos.js"
+/* 
+<div class="produto">
+    <div class="nome_produto"></div>
+    <div class="categoria_produto"></div>
+    <div class="marca_produto"></div>
+    <div class="preco_produto"></div>
+    <div class="loja_produto"></div>
+    <div class="img_produto"></div>
+    <div class="estoque_produto"></div>
+</div> 
+*/
+
+export function add(){
+    console.log(` adicionado ao carrinho`)
+}
